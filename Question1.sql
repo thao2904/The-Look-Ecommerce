de9f53ed9eaa9ba40cdcf3914e310d8edcc34dc5
thao2904 <nguyenth@dickinson.edu>
2024-09-12 22:32:55 -0400

    Create Question1.sql

---
+++ Question1.sql
@@ -0,0 +1,10 @@
+-- Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng ( Từ 1/2019-4/2022)
+SELECT 
+    COUNT(order_id) AS total_order, 
+    COUNT(user_id) AS total_user, 
+    FORMAT_TIMESTAMP('%Y-%m', created_at) AS month_year
+FROM 
+    bigquery-public-data.thelook_ecommerce.orders
+WHERE created_at BETWEEN '2019-01-01' AND '2022-04-30'
+GROUP BY month_year
+ORDER BY month_year;
