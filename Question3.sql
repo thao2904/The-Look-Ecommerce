cd648ee6c649a0c21965042d41aefac3d5eb37c8
thao2904 <nguyenth@dickinson.edu>
2024-09-17 00:43:22 -0400

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,23 +1,23 @@
-CREATE TEMP TABLE temp_user_age AS(
-WITH youngest_data AS(
-SELECT first_name,last_name,gender,age,
-CASE WHEN age = MIN(age) OVER (PARTITION BY gender) THEN 'YOUNGEST'
-ELSE ''END AS tag
-FROM bigquery-public-data.thelook_ecommerce.users 
-WHERE created_at BETWEEN '2019-01-01' AND '2022-04-30')
-, oldest_data AS(
-SELECT first_name,last_name,gender,age,
-CASE WHEN age = MAX(age) OVER (PARTITION BY gender) THEN 'OLDEST'
-ELSE ''END AS tag
-FROM bigquery-public-data.thelook_ecommerce.users 
-WHERE created_at BETWEEN '2019-01-01' AND '2022-04-30')
-SELECT first_name, last_name, gender, age, tag FROM youngest_data
-WHERE tag = 'YOUNGEST'
-UNION DISTINCT 
-SELECT first_name, last_name, gender, age, tag FROM oldest_data 
-WHERE tag = 'OLDEST');
-
-SELECT gender,SUM(CASE WHEN tag = 'YOUNGEST' THEN 1 ELSE 0 END) AS youngest_count,
-SUM(CASE WHEN tag = 'OLDEST' THEN 1 ELSE 0 END) AS oldest_count
-FROM temp_user_age
-GROUP BY gender;
+-- Tìm các khách hàng trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính 
+-- CTE này tìm độ tuổi nhỏ nhất và lớn nhất theo từng giới tính
+-- CTE này đếm số lượng người dùng có độ tuổi nhỏ nhất theo từng giới tính trong khoảng thời gian từ 01-2019 đến 04-2022
+-- CTE này đếm số lượng người dùng có độ tuổi lớn nhất theo từng giới tính trong khoảng thời gian từ 01-2019 đến 04-2022
+WITH user_age AS
+(SELECT gender, 
+MIN(age) as youngest_age, MAX(age) as oldest_age
+FROM bigquery-public-data.thelook_ecommerce.users
+GROUP BY gender)
+, youngest_age1 AS
+(SELECT a.first_name, a.last_name, a.age, a.gender, 'youngest'AS tag
+FROM bigquery-public-data.thelook_ecommerce.users a
+JOIN user_age AS b ON a.gender = b.gender
+WHERE a.age = b.youngest_age
+AND a.created_at BETWEEN '2019-01-01' AND '2022-04-30'
+GROUP BY a.age, a.gender,a.first_name, a.last_name)
+, oldest_age1 AS
+(SELECT c.first_name, c.last_name, c.age, c.gender, 'oldest'AS tag
+FROM bigquery-public-data.thelook_ecommerce.users c
+JOIN user_age AS b ON c.gender = b.gender
+WHERE c.age = b.oldest_age
+AND c.created_at BETWEEN '2019-01-01' AND '2022-04-30'
+GROUP BY c.age, c.gender,c.first_name, c.last_name)
