49498e196bbe938f087bd84b54ef0b4ea57f26cc
thao2904 <nguyenth@dickinson.edu>
2024-09-18 14:22:46 -0400

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,10 +1,3 @@
-/*Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính ( Từ 1/2019-4/2022)
-Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
-Hint: Sử dụng UNION các KH tuổi trẻ nhất với các KH tuổi trẻ nhất 
-tìm các KH tuổi trẻ nhất và gán tag ‘youngest’  
-tìm các KH tuổi trẻ nhất và gán tag ‘oldest’ 
-Insight là gì? (Trẻ nhất là bao nhiêu tuổi, số lượng bao nhiêu? Lớn nhất là bao nhiêu tuổi, số lượng bao nhiêu)*/
-Note: Lưu output vào temp table rồi đếm số lượng tương ứng 
 
 WITH user_age AS (
   SELECT gender, 
