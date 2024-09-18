2516a8a6d8ac3d29b22d92b5912569e60cd8d036
thao2904 <nguyenth@dickinson.edu>
2024-09-18 11:36:31 -0400

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,23 +1,38 @@
--- Tìm các khách hàng trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính 
--- CTE này tìm độ tuổi nhỏ nhất và lớn nhất theo từng giới tính
--- CTE này đếm số lượng người dùng có độ tuổi nhỏ nhất theo từng giới tính trong khoảng thời gian từ 01-2019 đến 04-2022
--- CTE này đếm số lượng người dùng có độ tuổi lớn nhất theo từng giới tính trong khoảng thời gian từ 01-2019 đến 04-2022
-WITH user_age AS
-(SELECT gender, 
-MIN(age) as youngest_age, MAX(age) as oldest_age
-FROM bigquery-public-data.thelook_ecommerce.users
-GROUP BY gender)
-, youngest_age1 AS
-(SELECT a.first_name, a.last_name, a.age, a.gender, 'youngest'AS tag
-FROM bigquery-public-data.thelook_ecommerce.users a
-JOIN user_age AS b ON a.gender = b.gender
-WHERE a.age = b.youngest_age
-AND a.created_at BETWEEN '2019-01-01' AND '2022-04-30'
-GROUP BY a.age, a.gender,a.first_name, a.last_name)
-, oldest_age1 AS
-(SELECT c.first_name, c.last_name, c.age, c.gender, 'oldest'AS tag
-FROM bigquery-public-data.thelook_ecommerce.users c
-JOIN user_age AS b ON c.gender = b.gender
-WHERE c.age = b.oldest_age
-AND c.created_at BETWEEN '2019-01-01' AND '2022-04-30'
-GROUP BY c.age, c.gender,c.first_name, c.last_name)
+/*Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính ( Từ 1/2019-4/2022)
+Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
+Hint: Sử dụng UNION các KH tuổi trẻ nhất với các KH tuổi trẻ nhất 
+tìm các KH tuổi trẻ nhất và gán tag ‘youngest’  
+tìm các KH tuổi trẻ nhất và gán tag ‘oldest’ 
+Insight là gì? (Trẻ nhất là bao nhiêu tuổi, số lượng bao nhiêu? Lớn nhất là bao nhiêu tuổi, số lượng bao nhiêu)*/
+Note: Lưu output vào temp table rồi đếm số lượng tương ứng 
+
+WITH user_age AS (
+  SELECT gender, 
+         MIN(age) as youngest_age, 
+         MAX(age) as oldest_age
+  FROM bigquery-public-data.thelook_ecommerce.users
+  GROUP BY gender
+),
+youngest_age1 AS (
+  SELECT a.first_name, a.last_name, a.age, a.gender, 'youngest' AS tag
+  FROM bigquery-public-data.thelook_ecommerce.users AS a
+  JOIN user_age AS b ON a.gender = b.gender
+  WHERE a.age = b.youngest_age
+    AND a.created_at BETWEEN '2019-01-01' AND '2022-04-30'
+),
+oldest_age1 AS (
+  SELECT a.first_name, a.last_name, a.age, a.gender, 'oldest' AS tag
+  FROM bigquery-public-data.thelook_ecommerce.users AS a
+  JOIN user_age AS b ON a.gender = b.gender
+  WHERE a.age = b.oldest_age
+    AND a.created_at BETWEEN '2019-01-01' AND '2022-04-30'
+)
+-- Counting the youngest and oldest users by gender
+,youngest_oldest_age AS (
+  SELECT * FROM youngest_age1
+  UNION ALL 
+  SELECT * FROM oldest_age1
+)
+SELECT gender, tag, COUNT(*) AS count
+FROM youngest_oldest_age 
+GROUP BY gender, tag;
