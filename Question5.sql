63b88ef1bf1b3af02b875581423ad1bfe9c4353f
thao2904 <nguyenth@dickinson.edu>
2024-09-18 14:20:10 -0400

    Update Question5.sql

--- Question5.sql
+++ Question5.sql
@@ -4,7 +4,6 @@ Output: dates (yyyy-mm-dd), product_categories, revenue*/
 /*
 Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua ( giả sử ngày hiện tại là 15/4/2022)
 Output: dates (yyyy-mm-dd), product_categories, revenue*/
-WITH product_revenue AS(
 SELECT 
 FORMAT_TIMESTAMP('%Y-%m-%d', a.created_at) AS date,
 c.category AS product_categories, 
@@ -14,9 +13,4 @@ JOIN bigquery-public-data.thelook_ecommerce.orders AS b ON a.order_id = b.order_
 JOIN bigquery-public-data.thelook_ecommerce.products AS c ON a.id = c.id
 WHERE DATE(a.created_at) BETWEEN DATE_SUB(DATE('2022-04-15'), INTERVAL 3 MONTH) AND DATE('2022-04-15')
 GROUP BY  c.category, a.created_at 
-ORDER BY a.created_at)
-
-SELECT date, product_categories, 
-SUM(revenue) OVER (PARTITION BY product_categories ORDER BY date) AS category_revenue
-FROM product_revenue
-ORDER BY date, product_categories
+ORDER BY a.created_at
