bd29cdf650e3a7ce30d76a6ad53d8137d886e9a7
thao2904 <nguyenth@dickinson.edu>
2024-09-19 18:01:03 -0400

    Update Question5.sql

--- Question5.sql
+++ Question5.sql
@@ -5,13 +5,21 @@ Output: dates (yyyy-mm-dd), product_categories, revenue*/
 Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua ( giả sử ngày hiện tại là 15/4/2022)
 Output: dates (yyyy-mm-dd), product_categories, revenue*/
 SELECT 
-FORMAT_TIMESTAMP('%Y-%m-%d', a.created_at) AS date,
-c.category AS product_categories, 
-ROUND(SUM(a.sale_price * b.num_of_item),2) as revenue
-FROM bigquery-public-data.thelook_ecommerce.order_items AS a
-JOIN bigquery-public-data.thelook_ecommerce.orders AS b ON a.order_id = b.order_id
-JOIN bigquery-public-data.thelook_ecommerce.products AS c ON a.id = c.id
-WHERE DATE(a.created_at) BETWEEN DATE_SUB(DATE('2022-04-15'), INTERVAL 3 MONTH) AND DATE('2022-04-15')
-GROUP BY  c.category, a.created_at 
-ORDER BY a.created_at
--- Link file output: https://drive.google.com/drive/u/0/folders/1AhgFxZsS8JKTg1OypA-cI7uOpxtK0dqt
+    FORMAT_DATE('%Y-%m-%d', a.created_at) AS date,
+    c.category AS product_categories, 
+    ROUND(SUM(a.sale_price * b.num_of_item), 2) AS revenue
+FROM 
+    bigquery-public-data.thelook_ecommerce.order_items AS a
+JOIN 
+    bigquery-public-data.thelook_ecommerce.orders AS b 
+    ON a.order_id = b.order_id
+JOIN 
+    bigquery-public-data.thelook_ecommerce.products AS c 
+    ON a.id = c.id
+WHERE 
+    DATE(a.created_at) BETWEEN DATE_SUB(DATE('2022-04-15'), INTERVAL 3 MONTH) 
+    AND DATE('2022-04-15')
+GROUP BY  
+    c.category, FORMAT_DATE('%Y-%m-%d', a.created_at)
+ORDER BY 
+    date;
