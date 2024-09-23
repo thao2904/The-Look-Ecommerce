910fc6dfd671e5d90d53ad17481fc566f7b09c38
thao2904 <nguyenth@dickinson.edu>
2024-09-23 00:03:04 -0400

    Update Project2.2.sql

--- Project2.2.sql
+++ Project2.2.sql
@@ -1,20 +1,27 @@
+WITH aggregated_data AS (
+  SELECT 
+      FORMAT_DATE('%Y-%m', a.created_at) AS month,
+      b.category AS product_category, 
+      ROUND(SUM(a.sale_price), 2) AS TPV,
+      COUNT(a.order_id) AS TPO,
+      ROUND(SUM(b.cost), 2) AS total_cost,
+      ROUND(SUM(a.sale_price) - SUM(b.cost), 2) AS total_profit,
+      ROUND((SUM(a.sale_price) - SUM(b.cost)) / SUM(b.cost), 2) AS profit_to_cost_ratio
+  FROM bigquery-public-data.thelook_ecommerce.order_items a
+  JOIN bigquery-public-data.thelook_ecommerce.products b
+  ON a.id = b.id
+  GROUP BY month, b.category
+)
+
 SELECT 
-    FORMAT_DATE('%Y-%m', a.created_at) AS month,
-    b.category AS product_category, 
-    ROUND(SUM(a.sale_price), 2) AS TPV,
-    COUNT(a.order_id) AS TPO,
-    LAG(ROUND(SUM(a.sale_price), 2)) OVER(PARTITION BY b.category ORDER BY FORMAT_DATE('%Y-%m', a.created_at)) AS previous_revenue, 
-    LAG(COUNT(a.order_id)) OVER(PARTITION BY b.category ORDER BY FORMAT_DATE('%Y-%m', a.created_at)) AS previous_order,
-    ROUND(SUM(b.cost), 2) AS total_cost,
-    ROUND(SUM(a.sale_price) - SUM(b.cost), 2) AS total_profit,
-    ROUND((SUM(a.sale_price) - SUM(b.cost)) / SUM(b.cost), 2) AS profit_to_cost_ratio
-FROM 
-    `bigquery-public-data.thelook_ecommerce.order_items` a
-JOIN 
-    `bigquery-public-data.thelook_ecommerce.products` b 
-ON 
-    a.id = b.id
-GROUP BY 
-    FORMAT_DATE('%Y-%m', a.created_at), b.category
-ORDER BY 
-    FORMAT_DATE('%Y-%m', a.created_at);
+    month,
+    product_category,
+    TPV,
+    TPO,
+    LAG(TPV) OVER(PARTITION BY product_category ORDER BY month) AS previous_revenue,
+    LAG(TPO) OVER(PARTITION BY product_category ORDER BY month) AS previous_order,
+    total_cost,
+    total_profit,
+    profit_to_cost_ratio
+FROM aggregated_data
+ORDER BY month;
